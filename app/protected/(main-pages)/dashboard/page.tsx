f5eb75374fa05708d1a7292e77ffd56f1b59db29
RAlcala2 <ryanalcala10@gmail.com>
2024-11-28 12:25:05 -0500

    small styling changes to dashboard grid

--- app/protected/(main-pages)/dashboard/page.tsx
+++ app/protected/(main-pages)/dashboard/page.tsx
@@ -22,18 +22,30 @@ export default async function Dashboard() {
     <>
       <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
         <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-          <div className="aspect-video rounded-xl bg-muted/50">
-            <h1>Total balance</h1>
+          <div className="aspect-video rounded-xl bg-muted/50 px-6 py-5 sm:py-6 shadow outline outline-muted/70">
+            <h1 className="text-black/80 leading-none tracking-tight">
+              Total balance
+            </h1>
             <AnimatedCounter balance={account.balance} />
           </div>
-          <div className="aspect-video rounded-xl bg-muted/50"></div>
-          <div className="aspect-video rounded-xl bg-muted/50"></div>
+          <div className="aspect-video rounded-xl bg-muted/50 px-6 py-5 sm:py-6 shadow outline outline-muted/70">
+            <h1 className="text-black/80 leading-none tracking-tight">
+              Cashflow
+            </h1>
+          </div>
+          <div className="aspect-video rounded-xl bg-muted/50 px-6 py-5 sm:py-6 shadow outline outline-muted/70">
+            <h1 className="text-black/80 leading-none tracking-tight">
+              Your cards
+            </h1>
+          </div>
         </div>
-        <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
+        <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min shadow outline outline-muted/70">
           <BankChart />
         </div>
-        <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
-          Recent transactions
+        <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min px-6 py-5 sm:py-6 shadow outline outline-muted/70">
+          <h1 className="text-black/80 leading-none tracking-tight">
+            Recent transactions
+          </h1>
         </div>
       </div>
     </>
--- components/AnimatedCounter.tsx
+++ components/AnimatedCounter.tsx
@@ -4,7 +4,7 @@ import CountUp from "react-countup";
 
 export default function AnimatedCounter({ balance }: { balance: number }) {
   return (
-    <div>
+    <div className="mt-4 text-2xl lg:text-4xl xl:text-5xl">
       $<CountUp end={balance} duration={2.5} decimals={2} />
     </div>
   );
